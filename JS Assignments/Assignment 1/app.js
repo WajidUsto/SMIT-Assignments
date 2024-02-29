41699db6e48778c6623699b01c591bee741aebae
WajidUsto <Wajidusto@gmail.com>
2024-02-29 13:33:06 +0500

    initial commit

--- JS Assignments/Assignment 1/app.js
+++ JS Assignments/Assignment 1/app.js
@@ -1,9 +1,13 @@
 
 // Question 1 
 
-var num = 2 +prompt("Enter any number")
-if (num / 2) {
-  "This is not devisible by 3"
+document.write ("Your Age is " + age + "and your name is Wajid" )
+var  age  = 45 ;
+console.log(age);
+if (age >= 18) {
+  "You can drive"
+}else{
+  "You cannot Drive"
 }
 
 
