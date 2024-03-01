da497aefdd70b46057fa59a34ecb0d4fc378534f
WajidUsto <Wajidusto@gmail.com>
2024-03-01 12:59:22 +0500

    initial commit

--- JS Assignments/JS practice/app.js
+++ JS Assignments/JS practice/app.js
@@ -1,12 +1,46 @@
 
 
-function greetUser ( username , lastName, age){
-  console.log(username + lastName + age);
-} 
-greetUser ('wajid' + " " , 'Usto' + " " , 16)
+// function greetUser ( username , lastName, age){
+//   console.log(username + lastName + age);
+// } 
+// greetUser ('wajid' + " " , 'Usto' + " " , 16)
 
 
-function sum(num1 , num2) {
-  console.log("sum of number two is ==>" , num1+num2);
+// function sum(num1 , num2) {
+//   console.log("sum of number two is ==>" , num1+num2);
+// }
+// sum(50 , 20)
+
+
+// function percentage (marks1 , marks2 , marks3){
+// var totalmarks = 300
+// var obtainedmarks = marks1 + marks2+ marks3
+// return obtainedmarks / totalmarks * 100
+// }
+
+// var ismailperc = percentage(50 , 41 , 62 )
+// console.log(ismailperc);
+
+
+
+
+
+
+function percentage(marks1 , marks2 , marks3) {
+  var totalmarks = 300
+  var obtainedmarks = marks1 + marks2 + marks3
+  return(totalmarksmarks / obtainedmarks * 100)
 }
-sum(50 , 20)
+var perc = (33 , 50 , 10)
+document.write("your percentage is " + perc + " %"  + " keep focus on your study ! ")
+console.log(perc);
+
+
+
+
+
+
+
+
+
+
