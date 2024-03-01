d6df91735afdd8d76589f375ab152c7cd6a82d2a
WajidUsto <Wajidusto@gmail.com>
2024-03-01 12:41:26 +0500

    initial commit

--- JS Assignments/JS practice/app.js
+++ JS Assignments/JS practice/app.js
@@ -1,5 +1,12 @@
 
 
-function ismail (){
-  
+function greetUser ( username , lastName, age){
+  console.log(username + lastName + age);
+} 
+greetUser ('wajid' + " " , 'Usto' + " " , 16)
+
+
+function sum(num1 , num2) {
+  console.log("sum of number two is ==>" , num1+num2);
 }
+sum(50 , 20)
