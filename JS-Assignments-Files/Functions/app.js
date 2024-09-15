28c60545d3fdff9711505d1195c6ca2020b7e4af
Wajid Usto <wajidusto@gmail.com>
2024-09-15 16:55:19 +0500

    command added

--- JS-Assignments-Files/Functions/app.js
+++ JS-Assignments-Files/Functions/app.js
@@ -4,9 +4,35 @@
 // Q1 ANS
 // block Code
 
-let a = 5;
-let b = 5;
-function num() {
-    return a + b
+// let a = 5;
+// let b = 5;
+// function num() {
+//     return a + b
+// }
+// console.log(num());
+
+// Q2 ANS
+// Name Show
+
+// let first = prompt("Enter Your First Name!")
+// let last = prompt("Enter Your Last Name!")
+
+// function showName() {
+//     let Name = first + last
+//     return Name
+// }
+// console.log(showName());
+
+// Q3 ANS
+// Sum of Two Numbers
+
+let num1 = +prompt("Enter Num1")
+let num2 = +prompt("Enter Num2")
+
+function sum() {
+    let total = `Your Total Sum is ${num1 + num2}`
+    return total
 }
-console.log(num());
+console.log(sum());
+
+
