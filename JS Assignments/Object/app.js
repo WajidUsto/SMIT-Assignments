b2ee89eca93332b4279f8fecf547997605d1feb1
Wajid Usto <wajidusto@gmail.com>
2024-09-27 20:21:43 +0500

    command added

--- JS Assignments/Object/app.js
+++ JS Assignments/Object/app.js
@@ -1,10 +1,28 @@
 
 
-let obj = {
-    userName: "Wajid Usto",
-    email: "wajidusto@gmail.com",
-    password: "123456789",
-    isLoginIn: true
+// let obj = {
+//     userName: "Wajid Usto",
+//     email: "wajidusto@gmail.com",
+//     password: "123456789",
+//     isLoginIn: true,
+//     Arr: ["Ornage" , "Apple" , "Banana"]
+// }
+// obj.Arr.unshift("Updated!")
+// console.log(obj.password);
+// console.log(obj.Arr);
+
+
+
+
+const user = {
+        userName: 'Wajid Usto',
+        email: "wajidusto@gmail.com",
+        password: "123456789",
+        isLoginIn: true,
+        Arr: ["Ornage" , "Apple" , "Banana"],
+        greetUser: function () {
+            console.log(`Hello ${this.userName}`);
+        }
 }
-console.log(obj.password);
+console.log(user.userName);
 
